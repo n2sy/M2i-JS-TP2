97ed33f3e0c9b431732984c3890ad12a07733928
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-06-20 10:20:06 +0100

    final step

--- script.js
+++ script.js
@@ -5,25 +5,41 @@ const inputIntitule = document.getElementById("intitule");
 const inputSomme = document.getElementById("somme");
 const listeDepenses = document.getElementById("dep-list");
 const spanTotal = document.getElementById("total-dep");
+const inputFilter = document.getElementById("inpFilter");
 
 let somme = 0;
 
 btnAdd.addEventListener("click", () => {
   console.log(inputIntitule.value, inputSomme.value);
-  const newLi = document.createElement("li");
-  newLi.textContent = `${inputIntitule.value} : ${inputSomme.value} €`;
-  newLi.classList.add("list-group-item");
-  listeDepenses.appendChild(newLi);
+  if (inputIntitule.value == "" || inputSomme.value == "")
+    alert("Veuillez remplir les deux champs");
+  else {
+    const newLi = document.createElement("li");
+    newLi.textContent = `${inputIntitule.value} : ${inputSomme.value} €`;
+    newLi.classList.add("list-group-item");
+    listeDepenses.appendChild(newLi);
 
-  //somme = somme + inputSomme.value;
-  // somme += parseFloat(inputSomme.value);
-  // somme += Number(inputSomme.value);
-  somme += +inputSomme.value;
-  spanTotal.textContent = `${somme} €`;
-  effacer();
+    //somme = somme + inputSomme.value;
+    // somme += parseFloat(inputSomme.value);
+    // somme += Number(inputSomme.value);
+    somme += +inputSomme.value;
+    spanTotal.textContent = `${somme} €`;
+    effacer();
+  }
 });
 
 btnEffacer.addEventListener("click", effacer);
+
+inputFilter.addEventListener("input", () => {
+  for (const element of listeDepenses.children) {
+    let t = element.textContent.split(" ");
+    if (inputFilter.value != "") {
+      if (Number(inputFilter.value) > Number(t[2]))
+        element.style.backgroundColor = "red";
+      else element.style.backgroundColor = "green";
+    } else element.style.backgroundColor = "white";
+  }
+});
 
 function effacer() {
   inputIntitule.value = "";
